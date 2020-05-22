5f640dff37d53b696e633565f2a0470b321972b7
poi1924 <45979451+poi1924@users.noreply.github.com>
2020-05-22 14:47:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,17 +21,17 @@ if ( sizeof($request_array['events']) > 0 ) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
 
-
+        $text = $event['message']['text'];
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
+            // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+            'messages' => [['type' => 'text', 'text' => $text ]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
 
         echo "Result: ".$send_result."\r\n";
-        
     }
 }
 
@@ -53,5 +53,3 @@ function send_reply_message($url, $post_header, $post_body)
 
     return $result;
 }
-
-?>
