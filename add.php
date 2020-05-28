e6222e73d6360596dea70fa44bc803f0250041b3
poi1924 <45979451+poi1924@users.noreply.github.com>
2020-05-29 01:22:25 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,3 +1,29 @@
+
+<html>
+<head>
+<title>ThaiCreate.Com</title>
+</head>
+<body>
+<?php
+$servername = "us-cdbr-east-05.cleardb.net";
+$username = "b4b3b12394a0d8"";
+$password = "02d5790f";
+$objConnect = mysql_connect($host,$username,$password);
+
+if($objConnect)
+{
+	echo "MySQL Connected";
+}
+else
+{
+	echo "MySQL Connect Failed : Error : ".mysql_error();
+}
+
+mysql_close($objConnect);
+?>
+</body>
+</html>
+/*
 <?php
 $temp = $_GET['temp'];
 
@@ -23,3 +49,4 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 ?>
+*/
