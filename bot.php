e5fc06fa9b68cf836f23ef6df8f3aea3425d7c69
poi1924 <45979451+poi1924@users.noreply.github.com>
2020-05-29 01:03:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,10 +1,10 @@
 <?php
 $temp = $_GET['temp'];
 
-$servername = "us-cdbr-east-05.cleardb.net";
-$username = "b4b3b12394a0d8"";
-$password = "02d5790f";
-$dbname = "heroku_5652a807fc2262c";
+  $servername = "us-cdbr-east-05.cleardb.net";
+  $username = "b4b3b12394a0d8";
+  $password = "02d5790f";
+  $dbname = "heroku_5652a807fc2262c";
 // Create connection
 $conn = new mysqli($servername, $username,$password, $dbname);
 // Check connection
