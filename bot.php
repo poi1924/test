95548af5e537e0da409b74a8c0231c92cfd60048
poi1924 <45979451+poi1924@users.noreply.github.com>
2020-05-29 02:42:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,39 +1,27 @@
-<html>
-<head>
-<meta http-equiv="refresh" content="3">
-</head> 
-<body>
 <?php
+$temp = $_GET['temp'];
+
   $servername = "us-cdbr-east-05.cleardb.net";
   $username = "b4b3b12394a0d8";
   $password = "02d5790f";
   $dbname = "heroku_5652a807fc2262c";
 
+
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username,$password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+$val = $_GET['temp'];
+$sql = "INSERT INTO esp8266(temp) VALUES ($val);";
 
-$sql = "SELECT * FROM esp8266 ORDER by id DESC LIMIT 5";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-	echo "<table border='1'><th>ID</th><th>TEMP</th><th>DATETIME</th>";
-    while($row = $result->fetch_assoc()) {
-		echo "<tr>";
-		echo "<td>".$row['id']."</td>";
-		echo "<td>".$row['temp']."</td>";
-		echo "<td>".$row['day']."</td>";
-		echo "</tr>";
-    }
-	echo "</table>";
+if ($conn->query($sql) === TRUE) {
+    echo "save OK";
 } else {
-    echo "0 results";
+    echo "Error:" . $sql . "<br>" . $conn->error;
 }
+
 $conn->close();
 ?>
-</body>
-</html>
+
