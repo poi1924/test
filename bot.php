a06aebc5001968afc4687a32a4daf9f56bca522c
poi1924 <45979451+poi1924@users.noreply.github.com>
2020-05-29 02:01:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,24 +1,39 @@
 <html>
 <head>
-<title>ThaiCreate.Com</title>
-</head>
+<meta http-equiv="refresh" content="3">
+</head> 
 <body>
 <?php
-$servername = "us-cdbr-east-05.cleardb.net";
-$username = "b4b3b12394a0d8"";
-$password = "02d5790f";
-$objConnect = mysql_connect($host,$username,$password);
+  $servername = "us-cdbr-east-05.cleardb.net";
+  $username = "b4b3b12394a0d8";
+  $password = "02d5790f";
+  $dbname = "heroku_5652a807fc2262c";
 
-if($objConnect)
-{
-	echo "MySQL Connected";
-}
-else
-{
-	echo "MySQL Connect Failed : Error : ".mysql_error();
-}
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-mysql_close($objConnect);
+$sql = "SELECT * FROM esp8266 ORDER by id DESC LIMIT 5";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+	echo "<table border='1'><th>ID</th><th>TEMP</th><th>DATETIME</th>";
+    while($row = $result->fetch_assoc()) {
+		echo "<tr>";
+		echo "<td>".$row['id']."</td>";
+		echo "<td>".$row['temp']."</td>";
+		echo "<td>".$row['day']."</td>";
+		echo "</tr>";
+    }
+	echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
 </body>
 </html>
