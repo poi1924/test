ee8e4602542ae26038df18254d268695daf8c2b7
poi1924 <45979451+poi1924@users.noreply.github.com>
2020-05-29 02:11:13 +0700

    Update h.php

--- h.php
+++ h.php
@@ -1,16 +1,39 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="3">
+</head> 
+<body>
 <?php
-$access_token = 'BKaaM/SHph6Blvi1Ag4uIBab3hIOPVhQF3dsmUY47ca+9RiuGBgY0Lfs4XBEEmyo88AUWyKMUF4Lp2LpwHNINyPEBnrccRZuSiw1WNWfTLttE50JJsH4VEouiUD1WoYQtDdSw1b8sWZFY+UEuMFlUQdB04t89/1O/w1cDnyilFU=';
+  $servername = "us-cdbr-east-05.cleardb.net";
+  $username = "b4b3b12394a0d8";
+  $password = "02d5790f";
+  $dbname = "heroku_5652a807fc2262c";
 
-$url = 'https://api.line.me/v2/oauth/verify';
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$sql = "SELECT * FROM esp8266 ORDER by id DESC LIMIT 5";
+$result = $conn->query($sql);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-
-echo $result;
+if ($result->num_rows > 0) {
+    // output data of each row
+	echo "<table border='1'><th>ID</th><th>TEMP</th><th>DATETIME</th>";
+    while($row = $result->fetch_assoc()) {
+		echo "<tr>";
+		echo "<td>".$row['id']."</td>";
+		echo "<td>".$row['temp']."</td>";
+		echo "<td>".$row['day']."</td>";
+		echo "</tr>";
+    }
+	echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
+</body>
+</html>
